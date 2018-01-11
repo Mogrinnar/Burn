5e8aca3f8b5dcdbc0ad3299cdac8ec6f0a78e64f
PC Jacob <pierreclaudejacob@gmail.com>
2018-01-11 15:32:07 -0500

    Added aiming

--- Burn/Assets/Player/Player.cs
+++ Burn/Assets/Player/Player.cs
@@ -6,27 +6,40 @@ public class Player : MonoBehaviour {
 
     public Vector3 initialWorldPos;
     [Range(1.0f, 10.0f)] public float speed = 1.0f;
+    public Weapon weapon;
 
     private Transform _transform;
+    private Transform _weaponTransform;
     private float _axisMoveInputX = 0.0f;
     private float _axisMoveInputY = 0.0f;
     private float _axisAimInputX = 0.0f;
     private float _axisAimInputY = 0.0f;
     private Vector3 _moveDirection = Vector3.zero;
+    private Vector3 _aimDirection = Vector3.zero;
     private Vector3 _currentWorldPos = Vector3.zero;
+
+    private float _previousAngle = 0.0f;
 
     void Start () {
         _transform = GetComponent<Transform>();
         _transform.position = initialWorldPos;
         _currentWorldPos = initialWorldPos;
+        _weaponTransform = weapon.GetComponent<Transform>();
     }
 	
 	void FixedUpdate () {
-        getAxisInput();
-        move();
+        GetAxisInput();
+        Move();
+        Aim();
     }
 
-    private void move()
+    private void Aim()
+    {
+        if(_aimDirection.magnitude > 0.7f)
+            _weaponTransform.LookAt(_aimDirection + _weaponTransform.position);
+    }
+
+    private void Move()
     {
         _currentWorldPos = _transform.position;
         _currentWorldPos.x += _moveDirection.x * Time.deltaTime * speed;
@@ -37,10 +50,13 @@ public class Player : MonoBehaviour {
     /// <summary>
 	/// Get Axis data of the joysick
 	/// </summary>
-	private void getAxisInput()
+	private void GetAxisInput()
     {
         _moveDirection.x = 0.0f;
         _moveDirection.z = 0.0f;
+        _aimDirection.x = 0.0f;
+        _aimDirection.z = 0.0f;
+
         if (Input.GetAxisRaw("Horizontal-LS") > 0.3 || Input.GetAxisRaw("Horizontal-LS") < -0.3)
         {
             _axisMoveInputX = Input.GetAxisRaw("Horizontal-LS");
@@ -53,15 +69,11 @@ public class Player : MonoBehaviour {
             _moveDirection.z = _axisMoveInputY;
         }
 
-        if (Input.GetAxisRaw("Horizontal-RS") > 0.3 || Input.GetAxisRaw("Horizontal-RS") < -0.3)
-        {
-            _axisAimInputX = Input.GetAxisRaw("Horizontal-RS");
-        }
+        _axisAimInputX = Input.GetAxisRaw("Horizontal-RS");
+        _aimDirection.x = _axisAimInputX;
 
-        if (Input.GetAxisRaw("Vertical-RS") > 0.3 || Input.GetAxisRaw("Vertical-RS") < -0.3)
-        {
-            _axisAimInputY = Input.GetAxisRaw("Vertical-RS");
-        }
+        _axisAimInputY = Input.GetAxisRaw("Vertical-RS");
+        _aimDirection.z = _axisAimInputY;
 
         _moveDirection.Normalize();
     }
