23d6192de1224e6a4fc356dcb83856973d1fcc43
PC Jacob <pierreclaudejacob@gmail.com>
2018-01-15 19:48:51 -0500

    first playable

---
+++ Burn/Assets/EnemyGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour {
+
+    public float _generationSpeed= 5.0f;
+    public Enemy[] _enemies;
+
+    private float _generationCooldown;
+
+    // Use this for initialization
+    void Start () {
+        _generationCooldown = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        _generationCooldown -= Time.deltaTime;
+        if(_generationCooldown < 0)
+        {
+            _generationCooldown = _generationSpeed;
+            foreach (Enemy enemy in _enemies)
+            {
+                SpawnEnemy(enemy);
+            }
+        }
+	}
+
+    private void SpawnEnemy(Enemy enemy)
+    {
+        var casing = Instantiate(enemy, transform.position, transform.rotation, null);
+    }
+}
